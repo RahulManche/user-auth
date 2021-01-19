c7a1b1104f3bf9ef47c857698e0d34f84972c97a
RahulManche <rahulmanche1@gmail.com>
2021-01-19 17:59:27 +0530

    Committed User class

---
+++ src/com/userauth/User.java
@@ -0,0 +1,12 @@
+package com.userauth;
+
+public class User {
+	String name;
+	String email;
+	String password;
+	User(String name,String email,String password){
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+}
